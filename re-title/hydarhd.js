9dd41b058e1bfd338452f57c0d4d642c21d51f34
krishna <krishna404@yandex.com>
2025-03-16 22:49:42 +0530

    add hydrahd

---
+++ re-title/hydarhd.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Retitle hydrahd
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-05
+// @description  Fix document title
+// @author       You
+// @match        https://hydrahd.me/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=netlify.app
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const getName = () => {
+    let name = document.title.split(" - ")[0].replaceAll(" ", "_");
+
+    const splitted = location.href.split("?")[0].split("/");
+    const season = splitted.at(-3);
+    const episode = splitted.at(-1);
+
+    if (season && episode) {
+      name = `${episode}_${name}_S${season}`;
+    }
+    return name;
+  };
+
+  const main = () => {
+    document.title = getName();
+  };
+
+  setTimeout(() => {
+    main();
+    let previousUrl = "";
+    const observer = new MutationObserver(function (mutations) {
+      if (location.href !== previousUrl) {
+        previousUrl = location.href;
+        main();
+      }
+    });
+    observer.observe(document, { subtree: true, childList: true });
+  }, 1000);
+})();
