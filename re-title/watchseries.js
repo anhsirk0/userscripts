64814e78f2d1a1ba5e2013733c89513b0e6ac30e
krishna <krishna404@yandex.com>
2025-03-18 13:06:33 +0530

    add watchseries

---
+++ re-title/watchseries.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Retitle watchseries
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-05
+// @description  Fix document title
+// @author       You
+// @match        https://watchseries.im/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=netlify.app
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const getName = () => {
+    let name = "";
+    const nameEl = document.querySelector(".heading-name>a");
+    if (nameEl) {
+      name = nameEl.innerHTML.replaceAll(" ", "_");
+      // name = (nameEl.dataset.jname ?? nameEl.innerHTML).replaceAll(" ", "_");
+    }
+    if (!name) {
+      const urlName = location.href.split("/").at(-1) ?? "Anime";
+      name = urlName.split("-").slice(0, -1).join("_");
+    }
+
+    const episodeEl = document.querySelector(".eps-item.active");
+    if (episodeEl) {
+      const match = /Eps ([0-9]+).*/.exec(episodeEl.title);
+      if (match) {
+        name = Number(match[1]) + "_" + name;
+      }
+    }
+
+    const seasonEl = document.querySelector("#current-season");
+    if (seasonEl) {
+      const match = /Season ([0-9]+).*/.exec(seasonEl.innerHTML);
+      if (match) {
+        name = name + "_S" + Number(match[1]);
+      }
+    }
+    return name;
+  };
+
+  const main = () => {
+    document.title = getName();
+  };
+
+  setTimeout(() => {
+    main();
+    let previousUrl = "";
+    const observer = new MutationObserver(function (mutations) {
+      if (location.href !== previousUrl) {
+        previousUrl = location.href;
+        main();
+      }
+    });
+    observer.observe(document, { subtree: true, childList: true });
+  }, 1000);
+})();
