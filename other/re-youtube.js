639e4f808a6ffac1cd78a8fc2f859267d1326171
krishna <krishna404@yandex.com>
2025-11-30 00:36:25 +0530

    feat: update yt script, add junk button to ignore channel

--- other/re-youtube.js
+++ other/re-youtube.js
@@ -11,39 +11,82 @@
 (function () {
   "use strict";
   const TITLE_SEL = ".yt-lockup-metadata-view-model__heading-reset";
+  const METADATA_SEL = ".yt-lockup-view-model__metadata";
   const CHANNEL_SEL = ".yt-content-metadata-view-model__metadata-row";
   const SHORTS_SEL = "ytd-rich-shelf-renderer";
   const INFO_SEL = ".yt-core-attributed-string";
+  const KEY = "yt-junk";
+
+  let NOT_INTERESTED = [
+    "some keywords",
+    "channel name",
+    "#tagname",
+    /some regex/,
+  ];
+
+  function addStyles() {
+    const style = document.createElement("style");
+    style.textContent = `.add-channel-to-blacklist-button {
+all: unset;
+position: absolute;
+bottom: 4px;
+right: 4px;
+padding: 4px 8px;
+background-color: #7030AF;
+color: #FFFFFF;
+box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
+display: none;
+cursor: pointer;
+border-radius: 4px;
+font-size: 1.6rem;
+}
+
+${METADATA_SEL}:hover .add-channel-to-blacklist-button {
+display: inline-block;
+}
+`;
+    document.head.append(style);
+  }
 
   function debounce(method, delay) {
     clearTimeout(method._tId);
     method._tId = setTimeout(method, delay);
   }
 
-  function handleIt(el) {
-    // if (el.dataset.processed === "1") return;
-    // el.dataset.processed = "1";
+  function getJunkChannelsFromLocalStorage() {
+    const namesStr = localStorage.getItem(KEY) || "";
+    try {
+      const parsed = JSON.parse(namesStr);
+      if (Array.isArray(parsed)) {
+        return parsed.filter((it) => typeof it === "string");
+      }
+    } catch {
+      return [];
+    }
+  }
 
+  function addBlacklistButton(el, channel) {
+    const container = el.querySelector(METADATA_SEL);
+    if (!container) return;
+    let button = document.createElement("button");
+    button.textContent = "Junk";
+    button.classList.add("add-channel-to-blacklist-button");
+    button.onclick = function () {
+      const junkChannels = getJunkChannelsFromLocalStorage().concat(channel);
+      localStorage.setItem(KEY, JSON.stringify(junkChannels));
+      el.remove();
+    };
+    container.appendChild(button);
+  }
+
+  function handleOne(el) {
     const title = el.querySelector(TITLE_SEL)?.innerText ?? "";
     const channel = el.querySelector(CHANNEL_SEL)?.innerText ?? "";
 
-    const notInterested = [
-      "Screen Rant",
-      "Ryan George",
-      "Pitch Meeting",
-      "Trevor Wallace",
-      "Doctor Mike",
-      "Comedy Central",
-      "Key & Peele",
-      "Samay",
-      "Chris and Jack",
-      "Brooklyn 99",
-      "AGC ANDY",
-    ];
-
-    let remove = notInterested.some(
-      (i) => channel.includes(i) || title.includes(i)
-    );
+    let remove = NOT_INTERESTED.some((it) => {
+      if (it instanceof RegExp) return it.test(channel) || it.test(title);
+      else return channel.includes(it) || title.includes(it);
+    });
 
     const views = Array.from(el.querySelectorAll(INFO_SEL))
       .filter((e) => e.innerText.includes("views"))
@@ -68,16 +111,20 @@
     if (remove) {
       console.log(`${channel}'s video removed: ${title}`);
       el.remove();
+    } else {
+      addBlacklistButton(el, channel);
     }
   }
 
   function handleAll(selector) {
     for (const el of document.querySelectorAll(selector)) {
-      handleIt(el);
+      handleOne(el);
     }
   }
 
   function main() {
+    addStyles();
+    getJunkChannelsFromLocalStorage().forEach((it) => NOT_INTERESTED.push(it));
     // const base = "https://www.youtube.com/watch?";
     const shorts = document.querySelector(SHORTS_SEL);
     if (shorts) {
